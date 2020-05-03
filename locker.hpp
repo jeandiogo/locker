9e481b302e12106a7141952577ec4cb6eb3dde69
Jango <jeandiogo@gmail.com>
2020-05-03 16:14:11 -0300

    Update locker.hpp

--- locker.hpp
+++ locker.hpp
@@ -21,7 +21,7 @@
 // Be aware that locking a file does not prevent other processes to modify the locked file or what it is protecting. The locking policy is only valid between programs using this library.
 // All methods will throw an exception if an empty filename is given or if the program does not have permission to modify the locked file or the directory the locked file is stored.
 // If the file to be locked does not exist, it will be created.
-// If you want to read or write to a locked file, you still have to open it using the input/output method you preffer, and it is your responsability to handle input/ouput data races among threads in your process.
+// If you want to read or write to a locked file, you still have to open it using the input/output method you preffer, and it is your responsability to handle input/ouput data races among threads inside your process.
 // It may be a good practice to create a separate lockfile to each file you intend to use (e.g. to open "a.txt" with exclusivity, first lock the file, say, "a.txt.lock").
 // If you opened a locked file, close it before unlocking.
 // Do not forget to unlock every file you have manually locked, and prefer to use the lock guard, which will automatically unlock the file before leaving current scope.
