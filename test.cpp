7fbd6c92af879afa6e712d56808c3112d47382aa
Jango <jeandiogo@gmail.com>
2020-06-19 18:14:24 -0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,19 @@
 #include "locker.hpp"
 
+#include <chrono>
+#include <cstdlib>
+#include <iostream>
+#include <thread>
+
 int main()
 {
+	auto const filename = "test.txt";
 	
+	auto my_guard = locker::lock_guard(filename);
+	
+	auto data = std::stoi(locker::xread<true>(filename));
+	
+	locker::xwrite<true>(filename, ++data);
+	
+	std::this_thread::sleep_for(std::chrono::milliseconds(10));
 }
