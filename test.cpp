4794545a6d4b4af948c1682690b203d2b9d6d75f
Jango <jeandiogo@gmail.com>
2020-06-20 23:02:05 -0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,18 +1,48 @@
 #include "locker.hpp"
 
+#include <sys/types.h>
+#include <unistd.h>
+
 #include <chrono>
+#include <iostream>
+#include <ranges>
 #include <string>
 #include <thread>
 
 int main()
 {
 	auto const filename = "test.txt";
+	std::ofstream(filename) << 0;
 	
-	auto const guard = locker::lock_guard(filename);
+	int pid;
+	for(auto i : std::views::iota(1, 11))
+	{
+		pid = fork();
+		if(pid < 0)
+		{
+			return EXIT_FAILURE;
+		}
+		else if(pid == 0)
+		{
+			break;
+		}
+	}
+	if(pid > 0)
+	{
+		return EXIT_SUCCESS;
+	}
 	
-	auto data = std::stoi(locker::xread<true>(filename));
+	std::this_thread::sleep_for(std::chrono::milliseconds(200));
 	
-	locker::xwrite<true>(filename, ++data);
+	auto guard = locker::lock_guard(filename);
 	
-	std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	int data;
+	std::ifstream(filename) >> data;
+	std::cout << "\nPID " << getpid() << ":\ndata read: " << data << "\nincrementing...";
+	std::ofstream(filename) << ++data;
+	std::cout << "\ndata written: " << data << std::endl;
+	
+	std::this_thread::sleep_for(std::chrono::milliseconds(200));
+	
+	return EXIT_SUCCESS;
 }
