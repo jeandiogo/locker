7d0b9eb1c8e58d253b3b00e080aa68bbcafd3534
Jango <jeandiogo@gmail.com>
2020-06-25 21:20:30 -0300

    Update locker.hpp

--- locker.hpp
+++ locker.hpp
@@ -21,7 +21,7 @@
 // The locking policy is guaranteed only among programs using this library. Thus, locking a file does not prevent other processes from opening it, but it ensures that only one at a time program will get the lock.
 // All locking and unlocking functions accept a single filename, a list of filenames, or a vector of filenames. If the file to be locked does not exist, it will be created.
 // An exception will be throw if an empty filename is given, if a directory name is given, or if the program does not have permission to read and write to the file or to the directory it is in.
-// A process will loose the lock if the lockfile is deleted. For this reason, if a file to be unlocked does not exist, an exception will be throw to indicate that a lock may have been lost during the execution, at some point after the lock.
+// A process will loose the lock if the lockfile is deleted. For this reason, if a file is not found when the unlock function is called, an exception will be throw to indicate that a lock may have been lost during the execution, at some point after the lock.
 // If you have manually locked a file, do not forget to unlock it. If for some reason you have locked a file twice, you have to unlock it twice too.
 // Therefore, always prefer using the lock guard, which will automatically unlock the file before leaving its scope of declaration.
 // The locker provides process-safety, but not thread-safety. Once a process has acquired the lock, neither its threads and future forks will be stopped by it, nor they will be able to mutually exclude each other by using the filelock.
