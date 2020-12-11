0d55463023ce5c5c118ec04e2307942db3cb33d8
Jango <jeandiogo@gmail.com>
2020-12-11 18:45:02 -0300

    Update test.cpp

---
+++ test.cpp
@@ -0,0 +1,81 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 
+// Locker (C++ Library)
+// Copyright (C) 2020 Jean Diogo (Jango) <jeandiogo@gmail.com>
+// 
+// Licensed under the Apache License Version 2.0 (the "License").
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at <http://www.apache.org/licenses/LICENSE-2.0>.
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and limitations under the License.
+// 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 
+// test.cpp
+// 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <thread>
+
+#define runtime_error(x) runtime_error("[" + std::string(__FILE__) + ":" + std::string(__func__) + ":" + std::to_string(__LINE__) + "] " + (x))
+
+#include "locker.hpp"
+
+#define NUM_FORKS 50
+
+inline auto safe_open(std::string const & filename, std::ios_base::openmode const mode)
+{
+	auto file = std::fstream(filename, mode);
+	if(!file.good())
+	{
+		throw std::runtime_error("could not open file \"" + filename + "\"");
+	}
+	return file;
+}
+
+int main()
+{
+	int data = 0;
+	std::string const filename = "test.txt";
+	safe_open(filename, std::fstream::out) << data << std::flush;
+	std::cout << "Process " << getpid() << " was initialized \"" << filename << "\" with value 0. Expecting " << NUM_FORKS << " at the end of the test." << std::endl;
+	
+	for(std::size_t i = 0; i < NUM_FORKS; ++i)
+	{
+		auto pid = fork();
+		if(pid < 0)
+		{
+			throw std::runtime_error("fork did not work");
+		}
+		else if(pid == 0)
+		{
+			auto const guard = locker::lock_guard(filename);
+			safe_open(filename, std::fstream::in) >> data;
+			auto const new_data = data + 1;
+			safe_open(filename, std::fstream::out) << new_data << std::flush;
+			std::cout << "Child " << getpid() << " read " << data << " and wrote " << new_data << std::endl;
+			std::this_thread::sleep_for(std::chrono::milliseconds(50));
+			return EXIT_SUCCESS;
+		}
+		else if(i == NUM_FORKS - 1)
+		{
+			int status;
+			while((pid = wait(&status)) > 0);
+			auto const guard = locker::lock_guard(filename);
+			safe_open(filename, std::fstream::in) >> data;
+			std::cout << "The test was" << (data != NUM_FORKS ? " not" : "") << " successful!" << std::endl;
+			return EXIT_SUCCESS;
+		}
+	}
+}
