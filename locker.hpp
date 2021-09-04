743c7148a0bb0518f2b9f928913d0b5e3ded7a54
Jango <jeandiogo@gmail.com>
2021-09-04 12:47:09 -0300

    Update locker.hpp

--- locker.hpp
+++ locker.hpp
@@ -28,16 +28,16 @@
 // 
 // locker::lock_guard_t my_lock = locker::lock_guard("a.lock");              //locks a file and automatically unlocks it before leaving current scope (an empty lockfile will be created if it does not exist)
 // locker::lock_guard_t my_lock = locker::lock_guard<true>("a.lock");        //use first template argument to make it "non-blocking" (i.e. will throw instead of wait if file is already locked)
-// locker::lock_guard_t my_lock = locker::lock_guard<false, true>("a.lock"); //use second template argument to not delete empty lockfiles (by default empty lockfiles are erased at destruction)// 
+// locker::lock_guard_t my_lock = locker::lock_guard<false, true>("a.lock"); //use second template argument to not delete empty lockfiles (by default empty lockfiles are erased at destruction) 
 // 
 // std::string       my_data = locker::xread("a.txt");                       //exclusively reads a file and returns its content as a string (returns an empty string if file does not exist)
 // std::string       my_data = locker::xread<true>("a.txt");                 //use template argument to remove trailing newlines ("\n" and "\r\n")
 // std::vector<char> my_data = locker::xread<char>("a.txt");                 //use template typename to get file content as a vector of some specified type
 // std::vector<int>  my_data = locker::xread<int>("a.txt");                  //note that in these cases trailing bytes will be ignored if the size of the file is not a multiple of the size of the chosen type
 // std::vector<long> my_data = locker::xread<long>("a.txt");                 //also note that an eventual traling newline may be included if it turns the file size into a multiple of the type size
-// locker::xread("a.txt", my_container);                                     //opens input file and calls operator ">>" once from the filestream to the container passed as argument
+// locker::xread("a.txt", my_container);                                     //inserts data from a file (std::fstream) into a container via a single call of the ">>" operator
 // 
-// locker::xwrite("a.txt", my_data);                                         //exclusively writes formatted data to a file (data type must be insertable to std::fstream via a single call to operator "<<")
+// locker::xwrite("a.txt", my_data);                                         //inserts data from a container into a file (std::fstream) via a single call of the "<<" operator
 // locker::xwrite("a.txt", "value", ':', 42);                                //exclusively writes multiple data to file
 // locker::xwrite<true>("a.txt", "order", ':', 66);                          //use first template argument to append data instead of overwrite
 // locker::xwrite<false, true>("a.txt", "foobar");                           //use second template argument to write a trailing newline
@@ -335,14 +335,13 @@ class locker
 	static auto xread(std::string const & filename)
 	{
 		auto const guard = lock_guard(filename);
-		std::vector<T> data;
 		auto input = std::fstream(filename, std::fstream::in | std::fstream::ate | std::fstream::binary);
 		if(!input.good())
 		{
 			throw std::runtime_error("could not open file \"" + filename + "\" for binary input");
 		}
-		data.resize(static_cast<std::size_t>(input.tellg()) / sizeof(T));
-		if(data)
+		auto data = std::vector<T>(static_cast<std::size_t>(input.tellg()) / sizeof(T));
+		if(data.size())
 		{
 			input.seekg(0);
 			input.read(reinterpret_cast<char *>(data.data()), static_cast<long>(data.size() * sizeof(T)));
